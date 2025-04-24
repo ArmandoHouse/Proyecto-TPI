8ee05af49816ea9b4270b51a89113993ab3f5e8c
user <ayalaunne@gmail.com>
2025-04-24 18:32:05 -0300

    .

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -1,69 +1,113 @@
 <?= $this->extend('layouts/plantilla') ?>
 
 <?= $this->section('titulo') ?>
-El Taller - Venta de Hardware
+ZonaHW - Venta de Hardware
 <?= $this->endSection() ?>
 
 <?= $this->section('contenido') ?>
 
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
+<div class="container-fluid">
+  <div id="carrousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="1000">
+        <img src="assets/img/slide4.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="500">
+        <img src="assets/img/slide2.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/slide3.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carrousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carrousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="assets/img/ryzen7.jpg" class="d-block w-100" alt="slide 1">
-    </div>
-    <div class="carousel-item">
-      <img src="assets/img/keyboard.jpg" class="d-block w-100" alt="slide 2">
-    </div>
-    <div class="carousel-item">
-      <img src="assets/img/gaming-setup.jpg" class="d-block w-100" alt="slide 3">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </button>
 </div>
 
-<section class="py-5 .inicio">
-  <div class="container">
-    <h2 class="text-center mb-4">Productos Destacados</h2>
-    <div class="row">
-      <div class="col-md-4 mb-4">
-        <div class="card h-100">
-          <img src="assets/img/admryzen.jpeg" class="card-img-top" alt="Producto 1">
-          <div class="card-body">
-            <h5 class="card-title">Procesador AMD Ryzen</h5>
-            <p class="card-text">Alto rendimiento para gaming y productividad.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100">
-          <img src="assets/img/kingston480.jpeg" class="card-img-top" alt="Producto 2">
-          <div class="card-body">
-            <h5 class="card-title">SSD Kingston 480GB</h5>
-            <p class="card-text">Velocidad y confiabilidad para tu equipo.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100">
-          <img src="assets/img/ram16gb.jpg" class="card-img-top" alt="Producto 3">
-          <div class="card-body">
-            <h5 class="card-title">Memoria RAM 16GB DDR4</h5>
-            <p class="card-text">Ideal para multitarea y cargas intensas.</p>
-          </div>
+<div class="container my-5">
+  <h2 class="mb-4 text-center">Productos Destacados</h2>
+
+  <div class="row">
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" lass="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
         </div>
       </div>
     </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" lass="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" lass="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" lass="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card h-100">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Producto 1</h5>
+        </div>
+      </div>
+    </div>
+
   </div>
-</section>
+
+</div>
 
 <?= $this->endSection() ?>
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -1,38 +1,43 @@
+<style>
+  .btn {
+    background-color:white;
+    border: none;
+    border-radius: 50%;
+  }
+</style>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="#">El Taller</a>
+
+    <a class="navbar-brand" href="#">Logo</a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <!-- Enlaces principales -->
-      <ul class="navbar-nav me-auto">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">      
+      <ul class="navbar-nav mx-auto">
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url('/public') ?>">Inicio</a>
+          <a class="nav-link" href="#" >Principal</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url('/public/productos') ?>">Productos</a>
+          <a class="nav-link" href="#">Quienes Somos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url('/public/envios-y-pagos') ?>">Envios y Pagos</a>
+          <a class="nav-link" href="#">Comercialización</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('/public/nosotros') ?>">Nosotros</a>
+          <a class="nav-link" href="#">Información de Contacto</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url('/public/contacto') ?>">Contacto</a>
+          <a class="nav-link" href="#">Términos y Usos</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= base_url('/public/terminos') ?>">Terminos</a>
-        </li>        
       </ul>
-
-      <!-- Barra de búsqueda -->
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Buscar">
-        <button class="btn btn-outline-light" type="submit">Buscar</button>
-      </form>
     </div>
-  </div>
+
+    <div class="mx-2">
+      <button type="button" class="btn"><span class="fa-solid fa-magnifying-glass"></span></button>
+      <button type="button" class="btn"><span class="fa-solid fa-cart-shopping"></span></button>
+      <button type="button" class="btn"><span class="fa-solid fa-user"></span></button>
+    </div>
 </nav>
